9f22645230bd24cc739ec84e72d6e2be0c881925
Alex Kramer <alex9jk@gmail.com>
2018-08-19 12:16:05 -0400

    formating

--- src/Button.js
+++ src/Button.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 
 class Button extends Component {
-    constructor(props){
-        super(props)
-       
-    }
-
-
-    render(){
-        return(
-          <div className={this.props.className}  id={this.props.val} onClick={this.props.buttonClicked}>
-            {this.props.display}
-          </div>
+    render() {
+        return (
+            <div className={this.props.className} id={this.props.val} onClick={this.props.buttonClicked}>
+                {this.props.display}
+            </div>
         )
     }
 }
--- src/Display.js
+++ src/Display.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 
 class Display extends Component {
-    constructor(props){
-        super(props)
-    }
     render(){
         return(
             <div id ="display">
